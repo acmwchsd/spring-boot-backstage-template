e3cc1add65a01285505b57a3e4c01999fc87803a
Visal Som <som.visal@wingbank.com.kh>
2025-12-03 15:34:46 +0700

    test spring template

---
+++ src/test/java/__packagePath__/KompotApplicationTests.java
@@ -0,0 +1,13 @@
+package __packagePath__;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KompotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
